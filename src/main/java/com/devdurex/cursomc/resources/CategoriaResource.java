a270af771b22de8e2d7ece4d27c5563d14d62446
durexex <eduardodurieuxlopes@gmail.com>
2022-03-08 15:44:44 -0300

    CategoriaDTO GET

--- src/main/java/com/devdurex/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/devdurex/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.devdurex.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devdurex.cursomc.domain.Categoria;
+import com.devdurex.cursomc.dto.CategoriaDTO;
 import com.devdurex.cursomc.services.CategoriaService;
 
 @RestController
@@ -28,6 +31,14 @@ public class CategoriaResource {
 				
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+				
+		return ResponseEntity.ok().body(listDTO);
+	}	
 		
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
