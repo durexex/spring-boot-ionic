09a2e251874a63b68c14ca0d6fb742b784eea40f
durexex <eduardodurieuxlopes@gmail.com>
2022-03-08 21:55:02 -0300

    Personalizando mensagem do erro de validação

--- src/main/java/com/devdurex/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/devdurex/cursomc/resources/exception/StandardError.java
@@ -1,10 +1,41 @@
 package com.devdurex.cursomc.resources.exception;
 
-public class StandardError {
-	
+import java.io.Serializable;
+
+public class StandardError implements Serializable  {
+	private static final long serialVersionUID = 1L;
+
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
+	
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public StandardError() {
+		
+	}
 	
 	public StandardError(Integer status, String msg, Long timeStamp) {
 		super();
