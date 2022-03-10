156ca689c1139fb3ba86343722992709ed3e3955
durexex <eduardodurieuxlopes@gmail.com>
2022-03-10 14:22:01 -0300

    Validação personalizada email no update

---
+++ src/main/java/com/devdurex/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.devdurex.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.devdurex.cursomc.domain.Cliente;
+import com.devdurex.cursomc.dto.ClienteDTO;
+import com.devdurex.cursomc.repositories.ClienteRepository;
+import com.devdurex.cursomc.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator  implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+				
+		Cliente clienteAuxiliar = clienteRepository.findByEmail(objDto.getEmail());
+		if (clienteAuxiliar != null && !clienteAuxiliar.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "e-Mail já cadastrado"));	
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
