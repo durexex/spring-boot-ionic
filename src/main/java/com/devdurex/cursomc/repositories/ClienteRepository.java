ef2a56f930d416972f55aa79b703b85b3f477cbe
durexex <eduardodurieuxlopes@gmail.com>
2022-03-10 14:04:34 -0300

    Validação personalizada email

--- src/main/java/com/devdurex/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/devdurex/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,16 @@ package com.devdurex.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devdurex.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
+	
+	
 
 }
--- src/main/java/com/devdurex/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/devdurex/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.devdurex.cursomc.domain.Cliente;
 import com.devdurex.cursomc.domain.enums.TipoCliente;
 import com.devdurex.cursomc.dto.ClienteNewDTO;
+import com.devdurex.cursomc.repositories.ClienteRepository;
 import com.devdurex.cursomc.resources.exception.FieldMessage;
 import com.devdurex.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator  implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	
@@ -28,6 +35,11 @@ public class ClienteInsertValidator  implements ConstraintValidator<ClienteInser
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("CpfOuCnpj", "CNPJ inválido"));			
+		}
+		
+		Cliente clienteAuxiliar = clienteRepository.findByEmail(objDto.getEmail());
+		if (clienteAuxiliar != null) {
+			list.add(new FieldMessage("email", "e-Mail já cadastrado"));	
 		}
 		
 		for (FieldMessage e : list) {
