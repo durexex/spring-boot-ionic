db077ab018c1879d69b83cdc57850df348a2797c
durexex <eduardodurieuxlopes@gmail.com>
2022-04-09 14:39:56 -0300

    refresh token

--- src/main/java/com/devdurex/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/devdurex/cursomc/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.devdurex.cursomc.domain.Cliente;
 import com.devdurex.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -67,6 +68,25 @@ public abstract class AbstractEmailService implements EmailService {
 		
 	}
 	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEMail(cliente, newPass);
+		sendEmail(sm);		
+	}
+	
+	
+	protected SimpleMailMessage prepareNewPasswordEMail(Cliente cliente, String newPass) {
+
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha " + newPass);
+		
+		return sm;
+	}
+
 	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
 		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
